62ff8e569532cd74f50c71cedafebacb72a2fe73
Peter Akhigbe <akhigbe37@gmail.com>
2023-04-18 14:25:49 +0100

    create comment popup

--- src/index.js
+++ src/index.js
@@ -5,10 +5,36 @@ const showsList = document.querySelector('.shows');
 const showsCount = document.querySelector('.shows-count');
 
 let shows = [];
+
+const commentFunc = (array) => {
+  const commentBtns = document.querySelectorAll('.comments-btn');
+  const commentPopup = document.querySelector('.comment-popup');
+  const closeBtn = document.querySelector('.close-popup-btn');
+  const popupPhoto = document.querySelector('.popup-photo');
+  const movieTitle = document.querySelector('.movie-title');
+  const movieSummary = document.querySelector('.movie-summary');
+  // const submitCommentBtn = document.querySelector('.submit-comment-btn');
+
+  commentBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      commentPopup.style.display = 'flex';
+      popupPhoto.src = array[index].image.original;
+      movieTitle.textContent = array[index].name;
+      movieSummary.innerHTML = array[index].summary;
+    });
+  });
+
+  closeBtn.addEventListener('click', () => {
+    commentPopup.style.display = 'none';
+  });
+};
+
 const loadData = async () => {
   const data = await loadShows();
   shows = data;
   showsList.innerHTML = '';
+  // console.log(shows);
+
   shows.forEach((item) => {
     showsList.innerHTML += `
         <li class="show">
@@ -23,10 +49,12 @@ const loadData = async () => {
             </div>
           </div>
           <button class="comments-btn">Comments</button>
-        </li> 
+        </li>
       `;
   });
   showsCount.innerHTML = `TV Shows (${shows.length})`;
+
+  commentFunc(shows);
 };
 
 loadData();
