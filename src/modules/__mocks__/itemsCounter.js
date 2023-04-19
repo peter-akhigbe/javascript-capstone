be248667ebaca70e6cf408ffca42468b9d7f5e87
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-04-19 09:22:11 +0000

    Add itemsCounter.js in mocks directory

---
+++ src/modules/__mocks__/itemsCounter.js
@@ -0,0 +1,12 @@
+const countItems = () => {
+  Promise.resolve([
+    { id: 1, name: 'Under the dome' },
+    { id: 2, name: 'Person of interest' },
+    { id: 3, name: 'Bitten' },
+    { id: 4, name: 'Arrow' },
+    { id: 5, name: 'True Detective' },
+    { id: 6, name: 'The 100' },
+  ]);
+};
+
+export default countItems;
